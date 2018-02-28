893367a59840ab15a6db0f633ed1c11684200414
Rastislav Valovic <ravalovic@hotmail.com>
2018-02-28 22:01:43 +0100

    Backup scripts refactor

--- L4S/backup.ps1
+++ L4S/backup.ps1
@@ -1,32 +1,39 @@
 # backup L4S enviroment (c) BlueZ, s.r.o 2018
 # version: 1.0
 #SET up variables
+    #script directory 
     $myRunDirectory = $PSScriptRoot;
+    $myConfigFile = $myRunDirectory+"\backup.config"
+    #Parameters form backup.config - xml file
+	if( -Not (Test-Path -Path $myConfigFile ) )
+	{
+	    write-host "Config file  $myConfigFile missing. Create it!" -ForegroundColor "DarkRed";
+		exit 1;
+	} 
 	[xml]$config = Get-Content "$myRunDirectory\backup.config";
+
+    #commonSettings Section
 	$backupDirectory = $config.configuration.commonSettings.backupDirectory;
+    $backL4S = $backupDirectory+"\application";
+	$backDB = $backupDirectory+"\database";
 	[int]$DayOfRun = $config.configuration.commonSettings.dayOfRun;
 	[int]$Days = $config.configuration.commonSettings.delOlderAs;
 	$compress = $config.configuration.dbSettings.compressDBbackup;
 	if($compress.ToUpper() -eq "YES") { $CompressDbBackup=$true } else { $CompressDbBackup=$false }
-	$backL4S = $backupDirectory+"\application";
-	$backDB = $backupDirectory+"\database";
-	$PROD_L4SbinPath=$config.configuration.appSettings.PROD_L4SPath;
-	$TEST_L4SbinPath=$config.configuration.appSettings.TEST_L4SPath;
-	$PROD_WebApp=$config.configuration.appSettings.PROD_WebApp;
-	$TEST_WebApp=$config.configuration.appSettings.TEST_WebApp;
+
+    #dbSettings section 
+    $dbSettingsSection = $config.GetElementsByTagName("dbSettings");
+    $databasesArray = $dbSettingsSection.GetElementsByTagName("database");
 	$DBServer=$config.configuration.dbSettings.databaseServer;
-	$databasesArray = $config.GetElementsByTagName("database");
+
+    #appSettings section 
+    $appSettings = $config.GetElementsByTagName("appSettings");
+    $applicationValues = $appSettings.GetElementsByTagName("add");
+    
 	#variable
 	$date = Get-Date -Format yyyyMMddHHmmss;
 	$dayOfWeek = [Int] (Get-Date).DayOfWeek;
-	#Archive names 
-	$PROD_L4SbinArchiveName="PROD_L4Sbin_archive_$($date).zip";
-	$TEST_L4SbinArchiveName="TEST_L4Sbin_archive_$($date).zip";
-	$PROD_VOSKarchiveName="PROD_VOSKweb_archive_$($date).zip";
-	$TEST_VOSKarchiveName="TEST_VOSKweb_archive_$($date).zip";
-	$DB_ArchiveNameBAK="DB_FULL_archive_$($date).zip";
-	$DB_ArchiveNameINC="DB_INC_archive_$($date).zip";
-
+	
 #Region Functions
 function CheckBackupDirectory(){
 	# Test if backup dir Exist if not create
@@ -42,62 +49,23 @@ function CheckBackupDirectory(){
 }
 
 function BackupApplication(){
-	$files = Get-ChildItem -Path "$($backL4S)\*.zip"
-	if (( $dayOfWeek -eq $DayOfRun ) -or ( $files.count -eq 0 )){
-		# Prod
-		$archiveName="$($backL4S)\$($PROD_L4SbinArchiveName)";
-		Compress-Archive -Path $PROD_L4SbinPath -DestinationPath $archiveName;
-		$archiveName="$($backL4S)\$($PROD_VOSKarchiveName)";
-		Compress-Archive -Path $PROD_WebApp -DestinationPath $archiveName;
-		#Test
-		$archiveName="$($backL4S)\$($TEST_L4SbinArchiveName)";
-		Compress-Archive -Path $TEST_L4SbinPath -DestinationPath $archiveName;
-		$archiveName="$($backL4S)\$($TEST_VOSKarchiveName)";
-		Compress-Archive -Path $TEST_WebApp -DestinationPath $archiveName;
-		
-		#delete old backups 
-		$Now = Get-Date
-		$Extension = "*.zip"
-		#----- define LastWriteTime parameter based on $Days ---#
-		$LastWrite = $Now.AddDays(-$Days)
-		#----- get files from App backup #
-		$Files = Get-Childitem $backL4S -Include $Extension -Recurse | Where {$_.LastWriteTime -le "$LastWrite"}
-		foreach ($File in $Files) 
-			{
-			if ($File -ne $NULL)
-				{
-				write-host "Deleting File $File" -ForegroundColor "DarkRed"
-				Remove-Item $File.FullName | out-null
-				}
-			else
-				{
-				Write-Host "No more files to delete!" -foregroundcolor "Green"
-				}
-			}
-	}
+	foreach ($app in $applicationValues)
+	{
+         $appName = $app.Key +"_archive_$($date).zip";
+         $appPath = $app.Value;
+         $archiveName="$($backL4S)\$($appName)";
+         if( Test-Path -Path $appPath ) 
+	      {
+            Compress-Archive -Path $appPath -DestinationPath $archiveName;
+          }
+	} 
 }
 function DatabaseFullBackup(){
 	foreach ($db in $databasesArray){
       $dbname = $db.InnerXML;
 	  Backup-SqlDatabase -ServerInstance $DBServer -Database $dbname -BackupFile "$($backDB)\$($dbname)_db_$($date).bak"
 	}
-	#----- get files from DB backup #	
-	$Now = Get-Date
-	$Extension = "*.[bz][ai][kp]"
-	$LastWrite = $Now.AddDays(-$Days)
-	$Files = Get-Childitem $backDB -Include $Extension -Recurse | Where {$_.LastWriteTime -le "$LastWrite"}
-	foreach ($File in $Files) 
-		{
-		if ($File -ne $NULL)
-			{
-			write-host "Deleting File $File" -ForegroundColor "DarkRed"
-			Remove-Item $File.FullName | out-null
-			}
-		else
-			{
-			Write-Host "No more files to delete!" -foregroundcolor "Green"
-			}
-		}
+	
 }
 
 function DatabaseIncBackup(){
@@ -112,6 +80,8 @@ function DatabaseIncBackup(){
 
 function CompressDbBackup(){
 #Create zip from db backup 
+    $DB_ArchiveNameBAK="DB_FULL_archive_$($date).zip";
+	$DB_ArchiveNameINC="DB_INC_archive_$($date).zip";
 	$archiveName="$($backDB)\$($DB_ArchiveNameBAK)";
 	$backDBfile="$($backDB)\*.bak";
 	$files = Get-ChildItem -Path $backDBfile;
@@ -132,17 +102,42 @@ function CompressDbBackup(){
 			}
 
 }
+function DeleteOldFiles([string]$delPath){
+    #----- get files from DB backup #	
+	$Now = Get-Date;
+	$Extension = "*.[bz][ai][kp]";
+	$LastWrite = $Now.AddDays(-$Days)
+	$Files = Get-Childitem $delPath -Include $Extension -Recurse | Where {$_.LastWriteTime -le "$LastWrite"}
+	foreach ($File in $Files) 
+		{
+		if ($File -ne $NULL)
+			{
+			write-host "Deleting File $File" -ForegroundColor "DarkRed";
+			Remove-Item $File.FullName | out-null;
+			}
+		else
+			{
+			Write-Host "No more files to delete!" -foregroundcolor "Green";
+			}
+		}
+}
 #end region
 
 # ******* MAIN ******
-CheckBackupDirectory
-BackupApplication
 # Full Backup Database # in saturday full backup #check if exist some backup
-$files = Get-ChildItem -Path "$($backDB)\*.[bz][ai][kp]"
-if (( $dayOfWeek -eq $DayOfRun ) -or ( $files.count -eq 0 )) {
-	DatabaseFullBackup
-} else {
-	DatabaseIncBackup
-}
-if ($CompressDbBackup){	CompressDbBackup }		
-Exit(0);
+	
+    CheckBackupDirectory;
+	$filesDB = Get-ChildItem -Path "$($backDB)\*.[bz][ai][kp]";
+	$filesAPP = Get-ChildItem -Path "$($backL4S)\*.zip";
+	
+	if (( $dayOfWeek -eq $DayOfRun ) -or (( $filesAPP.count -eq 0 ) -and ( $filesDB.count -eq 0 ))) {
+		BackupApplication;
+		DatabaseFullBackup;
+		if ($CompressDbBackup){	CompressDbBackup }
+		DeleteOldFiles $backL4S;
+		DeleteOldFiles $backDB;
+	} else {
+		DatabaseIncBackup;
+	}
+		
+    exit 0;
